eca3a9233c15e7a2f6373c194052c0c6c693b094
sirakisaias <sirakisaias@gmail.com>
2021-05-07 14:51:09 -0700

    finished step 1-4

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,24 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+  const headerDiv = document.createElement('div');
+  const spanDate = document.createElement('span');
+  const titleH1 = document.createElement('h1');
+  const spanTemp = document.createElement('span');
+
+  spanDate.textContent = date;
+  titleH1.textContent = title;
+  spanTemp.textContent = temp;
+
+  headerDiv.classList.add('header');
+  spanDate.classList.add('date');
+  spanTemp.classList.add('temp');
+
+  headerDiv.appendChild(spanDate);
+  headerDiv.appendChild(titleH1);
+  headerDiv.appendChild(spanTemp);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
@@ -20,6 +38,9 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+
+  const headerSelector = document.querySelector(selector);
+  headerSelector.appendChild(Header('Sirak Lambda Project', 'May 7', '72 Degrees'))
 }
 
 export { Header, headerAppender }
--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +14,17 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topic = document.createElement('div');
+  topic.classList.add('topics');
+
+  topics.forEach(item => {
+    const tab = document.createElement('div');
+    tab.textContent = item;
+    tab.classList.add('tab');
+    topic.appendChild(tab);
+  });
+
+  return topic;
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +35,19 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+
+  axios
+  .get('https://lambda-times-api.herokuapp.com/topics')
+  .then(res =>{
+    console.log(res)
+    const newTabs = document.querySelector(selector);
+    const newArray = res.data.topics;
+    newTabs.appendChild(Tabs(newArray))
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 }
 
 export { Tabs, tabsAppender }
